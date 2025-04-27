e6698a89cd7fe3aa7829ebc8b3e21813ab2dab3c
andres22 <afpinchao8@misena.edu.co>
2025-04-27 16:40:57 -0500

    make a first view undertand the fuction between laravel and react

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -24,6 +24,9 @@ export default function Authenticated({ user, header, children }) {
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
                                     Dashboard
                                 </NavLink>
+                                <NavLink href={route('cocktail')} active={route().current('cocktail')}>
+                                    Cocktail
+                                </NavLink>
                             </div>
                         </div>
 
--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -12,7 +12,14 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                             className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                         >
                             Dashboard
+                        </Link>,
+                        <Link
+                            href={route('cocktail')}
+                            className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                        >
+                            Cocktails
                         </Link>
+                        
                     ) : (
                         <>
                             <Link
