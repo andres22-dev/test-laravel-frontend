114882fc7219212b46cf868fb8c0d932e8ac7818
andres22 <afpinchao8@misena.edu.co>
2025-04-28 00:46:29 -0500

    adding fuction for save cocktail for each element of list

--- resources/js/Components/CardCocktail.jsx
+++ resources/js/Components/CardCocktail.jsx
@@ -1,4 +1,31 @@
+import { useEffect, useState } from 'react';
 export default function CardCocktail({ title, description, category, img }) {
+
+  const [isSaving, setIsSaving] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const saveCocktail = async () => {
+          setIsSaving(true);
+          setErrorMessage('');
+  
+          try {
+              const response = await axios.post('/api/cocktails', {
+                  name: title,  
+                  category: description,
+                  instructions: category,  
+                  image: img,  
+              });
+  
+              if (response.status === 201) {
+                  alert('Cóctel guardado con éxito!');
+              }
+          } catch (error) {
+              console.error('Error al guardar el cóctel:', error);
+              setErrorMessage('Hubo un error al guardar el cóctel.');
+          } finally {
+              setIsSaving(false);
+          }
+      };
+
   return (
     <div className="bg-white shadow-md rounded-lg p-4">
       <h2 className="text-xl font-bold mb-2">{title}</h2>
--- resources/js/Pages/Cocktail.jsx
+++ resources/js/Pages/Cocktail.jsx
@@ -7,11 +7,13 @@ export default function Cocktail({ auth }) {
 
 const [cocktails, setCocktails] = useState([]);
 
+
+
 useEffect(() => {
     axios.get('/cocktails')
     .then(response => {
         console.log(response)
-        setCocktails(response.data.drinks); // guardas la lista de cocktails
+        setCocktails(response.data.drinks); 
         
     })
     .catch(error => {
@@ -22,6 +24,8 @@ useEffect(() => {
     
 
     return (
+
+        
         <AuthenticatedLayout
             user={auth.user}
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">List Cocktails</h2>}
